d7e37e4b003892f1d8ebca2271ebad9de5a8b48e
Michal Galas <michalgalas1989@gmail.com>
2024-08-26 12:25:26 +0200

    last commit

--- MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1bd01f0b04efe121cf824614186ae233d504d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dea5eca83b7a18768f23f6803cbaac47536db45")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
